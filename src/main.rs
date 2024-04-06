aa1fb796dc4017b45d5aaa2d024661f4b4e74bf1
Madżøng <ytnaintro@gmail.com>
2024-04-06 21:53:55 +0200

    bobm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::{settings::{Backends, RenderCreation, WgpuSettings}, RenderPlugin}};
 use plugin::plugin::HelloPlugin;
 
 mod plugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, HelloPlugin))
+        .add_plugins((DefaultPlugins.set(RenderPlugin {
+            render_creation: RenderCreation::Automatic(WgpuSettings {
+                backends: Some(Backends::DX12),
+                ..default()
+            }),
+            ..default()
+        }), HelloPlugin))
         .run();
 }
